6e875eec53c029b11a0aaa22c7253a8b04bb9cf4
GrahamRigby <grahamrigbyL@gmail.com>
2016-09-10 17:35:57 -0400

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginButton : UIButton!
     @IBOutlet weak var usernameInput : UITextField!
     @IBOutlet weak var passwordInput : UITextField!
+    @IBOutlet weak var createAccount : UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,9 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Checks username and password and switches to a logged in view controller or brings up an log in error message to continue
     @IBAction func login(sender : UIButton) {
-        if usernameInput.text == "Graham" && passwordInput.text == "Rigby" {
-            //Add Action that takes it to the next screen to log in
+        if (usernameInput.text == "Graham") && (passwordInput.text == "Rigby") {
+            let View2 = storyboard?.instantiateViewControllerWithIdentifier("Name Later")
+            self.presentViewController(View2!, animated: true, completion: nil)
+        }
+        else {
+           let alert = UIAlertController(title: "Login Failed", message: "Username or password is incorrect", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
         }
     }
-  }
+    
+    //Switches to a new view controller to create an account
+    @IBAction func createView(sender : UIButton!) {
+        let View3 = storyboard?.instantiateViewControllerWithIdentifier("Name Later")
+        self.presentViewController(View3!, animated: true, completion: nil)
+    }
+}
